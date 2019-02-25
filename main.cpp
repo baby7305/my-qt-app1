2fd3dba8a126981c8e4f08c8a7d4b39adefe2d07
baby7305 <songhaibei7305@163.com>
2019-02-25 16:25:33 +0800

    加上注释

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <QApplication>
-#include <QWidget>
+#include <QApplication>     //应用程序抽象类
+#include <QWidget>          //窗口类
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    QWidget widget;
-    widget.show();
-    return app.exec();
+    QWidget widget;     //构造一个窗口
+    widget.show();      //显示窗口
+    return app.exec();  //在exec中有一个消息循环
 }
