5ae8480c74768721f3efaa7475f68300622fa25d
baby7305 <songhaibei7305@163.com>
2019-02-25 17:01:50 +0800

    标题、窗口、按钮、事件信号槽，这下都齐备了

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <QApplication>     //应用程序抽象类
 #include <QWidget>          //窗口类
+#include <QPushButton>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QWidget widget;     //构造一个窗口
+    widget.setWindowTitle("hello world");
+
+    QPushButton pushButton;
+    pushButton.setText("button");
+    pushButton.setParent(&widget);
+
+    QObject::connect(&pushButton, SIGNAL(clicked()), &widget, SLOT(close()));
+
     widget.show();      //显示窗口
     return app.exec();  //在exec中有一个消息循环
 }
