896240a33c80531f112a4f1c4d31498f3cbfc0d3
baby7305 <songhaibei7305@163.com>
2019-02-26 13:49:42 +0800

    layout嵌套

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <QWidget>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -13,6 +15,26 @@ int main(int argc, char *argv[]) {
     QPushButton pushButton;
     pushButton.setText("button");
 
+    QGridLayout layout;
+    layout.setColumnStretch(3, 1);
+    layout.setRowStretch(4, 1);
+    layout.setColumnStretch(0, 1);
+    layout.setRowStretch(0, 1);
+
+    QLineEdit *password;
+    layout.addWidget(new QLabel("Username:"), 1, 1);
+    layout.addWidget(new QLineEdit(), 1, 2);
+    layout.addWidget(new QLabel("Password:"), 2, 1);
+    layout.addWidget(password = new QLineEdit(), 2, 2);
+    password->setEchoMode(QLineEdit::Password);
+
+    QHBoxLayout *hBox;
+    layout.addLayout(hBox = new QHBoxLayout, 3, 2);
+    hBox->addStretch(1);
+    hBox->addWidget(new QPushButton("Login"));
+
+
     widget.show();
+    widget.setLayout(&layout);
     return app.exec();
 }
