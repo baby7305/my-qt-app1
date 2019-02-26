6b4470127ead26dc02910bf29175054a0f38bd9d
baby7305 <songhaibei7305@163.com>
2019-02-26 09:55:20 +0800

    输入补全配置

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QWidget>
 #include <QtWidgets/QLineEdit>
+#include <QtWidgets/QCompleter>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -9,6 +10,10 @@ int main(int argc, char *argv[]) {
     QLineEdit lineEdit;
     lineEdit.show();
     lineEdit.setParent(&widget);
+
+    QCompleter completer(QStringList() << "aab" << "123" << "998");
+    completer.setFilterMode(Qt::MatchContains);
+    lineEdit.setCompleter(&completer);
 
     widget.show();
     return app.exec();
