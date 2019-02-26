65d5fc5cee398360fb8881cdcfb500e758e6135e
baby7305 <songhaibei7305@163.com>
2019-02-26 11:09:37 +0800

    准备按钮、输入框组件

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <QApplication>
 #include <QWidget>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QWidget widget;
+
+    QLineEdit lineEdit;
+    lineEdit.setPlaceholderText("要输入密码");
+    lineEdit.setParent(&widget);
+
+    QPushButton pushButton;
+    pushButton.setText("button");
+    pushButton.setParent(&widget);
+
     widget.show();
     return app.exec();
 }
