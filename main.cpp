7b20fdd452756e092b1b6d36bc51517ba3500c92
baby7305 <songhaibei7305@163.com>
2019-02-26 09:43:54 +0800

    加入基本配置

--- main.cpp
+++ main.cpp
@@ -10,6 +10,10 @@ int main(int argc, char *argv[]) {
     lineEdit.show();
     lineEdit.setParent(&widget);
 
+    lineEdit.setEchoMode(QLineEdit::PasswordEchoOnEdit);  //设置密码输入样式
+    lineEdit.text();    //获取输入内容
+    lineEdit.setPlaceholderText("要输入密码");
+
     widget.show();
     return app.exec();
 }
