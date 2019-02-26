c12be473b237ee60bc1353c9345f699f03b2768d
baby7305 <songhaibei7305@163.com>
2019-02-26 09:18:10 +0800

    初始化一个文本输入框

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
 #include <QWidget>
+#include <QtWidgets/QLineEdit>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QWidget widget;
+
+    QLineEdit lineEdit;
+    lineEdit.show();
+    lineEdit.setParent(&widget);
+
     widget.show();
     return app.exec();
 }
