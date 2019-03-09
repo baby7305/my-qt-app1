f6cb9fcdc7e50c0778649e698461d4e7539c4c13
baby7305 <songhaibei7305@163.com>
2019-03-09 18:05:47 +0800

    重载Event下MouseEvent中mousePressEvent方法，打印鼠标点击屏幕坐标

--- MyWidget.cpp
+++ MyWidget.cpp
@@ -1,6 +1,14 @@
 #include "MyWidget.h"
+#include <QDebug>
+#include <QMouseEvent>
 
 MyWidget::MyWidget(QWidget *parent) : QWidget(parent)
 {
 
+}
+
+void MyWidget::mousePressEvent(QMouseEvent *ev)
+{
+    QPoint pt = ev->pos();
+    qDebug() << pt;
 }
--- MyWidget.h
+++ MyWidget.h
@@ -8,6 +8,7 @@ class MyWidget : public QWidget
     Q_OBJECT
 public:
     explicit MyWidget(QWidget *parent = nullptr);
+    void mousePressEvent(QMouseEvent *);
 
 signals:
 
