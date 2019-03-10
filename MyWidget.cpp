5555609aeeec4e9b0d895079a0ed21e5f3ae25d0
baby7305 <songhaibei7305@163.com>
2019-03-10 15:28:41 +0800

    QLineEdit事件测试

--- MyWidget.cpp
+++ MyWidget.cpp
@@ -1,6 +1,17 @@
 #include "MyWidget.h"
+#include <QDebug>
+#include <QEvent>
+#include <QtWidgets/QVBoxLayout>
 
 MyWidget::MyWidget(QWidget *parent) : QWidget(parent)
 {
+    QVBoxLayout *lay = new QVBoxLayout(this);
+    lay->addWidget(edit = new QLineEdit());
+    connect(edit, SIGNAL(returnPressed()), this, SLOT(slotButtonClicked()));
+}
 
+void MyWidget::slotButtonClicked()
+{
+    QLineEdit *button = (QLineEdit *)sender();
+    qDebug() << button->text();
 }
--- MyWidget.h
+++ MyWidget.h
@@ -2,16 +2,19 @@
 #define MYWIDGET_H
 
 #include <QWidget>
+#include <QtWidgets/QLineEdit>
 
 class MyWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit MyWidget(QWidget *parent = nullptr);
+    QLineEdit* edit;
 
 signals:
 
 public slots:
+    void slotButtonClicked();
 };
 
 #endif // MYWIDGET_H
