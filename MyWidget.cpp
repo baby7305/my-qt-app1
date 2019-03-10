ae84a8f0f39fb76b82c2c88bae297f29b145ef16
baby7305 <songhaibei7305@163.com>
2019-03-10 23:11:41 +0800

    画一些简单的线、图形

--- MyWidget.cpp
+++ MyWidget.cpp
@@ -1,6 +1,16 @@
+#include <QtGui/QPainter>
 #include "MyWidget.h"
 
 MyWidget::MyWidget(QWidget *parent) : QWidget(parent)
 {
+}
 
+void MyWidget::paintEvent(QPaintEvent *)
+{
+    QPainter p(this);
+    p.drawLine(QPoint(0, 0), QPoint(100, 100));
+    p.drawEllipse(QPoint(95, 333), 50, 50);
+    p.drawText(QPoint(300, 50), "Hello world");
+    p.drawRect(QRect(40, 60, 100, 50));
+    p.drawRoundRect(QRect(80, 80, 150, 150));
 }
